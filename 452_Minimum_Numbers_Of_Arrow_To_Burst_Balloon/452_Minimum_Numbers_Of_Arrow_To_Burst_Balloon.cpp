27c73467c29fdf5d86d4fb58f8ea171a4093c403
hp-coderai <Harshkpatel93@gmail.com>
2025-04-30 17:22:29 -0400

    Create 452_Minimum_Numbers_Of_Arrow_To_Burst_Balloon.cpp

---
+++ 452_Minimum_Numbers_Of_Arrow_To_Burst_Balloon/452_Minimum_Numbers_Of_Arrow_To_Burst_Balloon.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        vector<vector<int>> arr;
+        sort(points.begin(),points.end());
+        int st = points[0][0];
+        int ed = points[0][1];
+        int cnt = 1;
+
+        for (int i = 0; i< points.size();i++) {
+            if ((points[i][0] >= st && points[i][0] <= ed) || 
+                (points[i][1] >= st && points[i][1] <= ed)) {
+                
+                st = max(st, points[i][0]);
+                ed = min(ed,points[i][1]);
+            }
+            else
+            {
+                st = points[i][0];
+                ed = points[i][1];
+                cnt++;
+            }
+        }
+        return cnt;   
+    }
+};
