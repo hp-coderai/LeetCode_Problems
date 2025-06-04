0c0dfb3638d1894d9fdee76b3e660a5ca432a241
hp-coderai <Harshkpatel93@gmail.com>
2025-06-03 21:15:28 -0400

    Create 1000_Find_Intersect_TTC.cpp

---
+++ 1000_Find_Intersect_TTC/1000_Find_Intersect_TTC.cpp
@@ -0,0 +1,27 @@
+class Vectors:
+   flaot x;
+   float y;
+
+bool findIntersect(Vectors* VecA, Vectors* VecB) {
+  bool Intersect  = false;
+  float SampleTime = 0.1;
+  unordered_map<int, pair<int, int>> map;
+  for(int i = 0; i<VecA.size(); i++){
+    map[i] = {VecA[i].x, VecA[i].y};
+  }
+  for(int i = 0; i<VecB.size(); i++){
+    if (VecB[i] in map) {
+      Intersect = true;
+      int idxB = i; 
+    }
+  }
+
+  for(int i = 0; i<VecB.size(); i++){
+    if (map[idxB] == {VecA[i].x, VecA[i].y}) { 
+      int idxA = i;   
+    }
+  }
+
+TTC = abs(idxA - idxB)*SampleTime;
+  
+}
